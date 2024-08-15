5ee875c5140db618d2ff066da5710e0c2837b2fc
amour-ingouma <115552799+amour-ingouma@users.noreply.github.com>
2024-08-15 15:52:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Knox</h1>
+        <h1>Hello Amour, tu es le plus beau de la terre</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Il faut Editer <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
